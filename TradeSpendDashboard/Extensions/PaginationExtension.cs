4686bd33b349e600c8326a7e776ec6733d64e365
agi2811 <88172290+agi2811@users.noreply.github.com>
2022-10-24 20:48:31 +0700

    Add files via upload

---
+++ TradeSpendDashboard/Extensions/PaginationExtension.cs
@@ -0,0 +1,15 @@
+﻿using TradeSpendDashboard.Models.Pagination;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TradeSpendDashboard.Extensions
+{
+    public static class PaginationExtension
+    {
+        public static async Task<PaginatedResult<T>> paginate<T>(this IQueryable<T> source,
+                                                PaginationParam param)
+        {
+            return await new PaginatedResult<T>(param).paginate(source);
+        }
+    }
+}
